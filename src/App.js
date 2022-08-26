8269ef749c31a4a2bd87ef8a94ca627da9247cf6
John4850 <johnnelson4850@gmail.com>
2022-08-26 12:13:29 -0700

    stamps added to components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ import basic_needs from './assets/bffa_icons/0_0_basic.png';
 import foundations from './assets/bffa_icons/1_0_foundations.png';
 import opportunity from './assets/bffa_icons/2_0_opportunity.png';
 
-import basicStamp from './assets/stamps/Basic_Knowledge_2.png'
+import advancedEducationStamp from './assets/stamps/Advanced_Education.png';
+import basicKnowledgeStamp from './assets/stamps/Basic_Knowledge_2.png';
+import environmentalQualityStamp from './assets/stamps/Environmental_Quality.png';
+import healthAndWellnessStamp from './assets/stamps/Health_And_Wellness.png';
+import inclusivenessStamp from './assets/stamps/Inclusiveness.png';
+import infoAndCommunicationsStamp from './assets/stamps/Info_And_Communications.png';
+import nutritionAndMedicalStamp from './assets/stamps/Nutrition_Medical_Care.png';
+import personalFreedomStamp from './assets/stamps/Personal_Freedom_and_Choice.png';
+import personalRightsStamp from './assets/stamps/Personal_Rights.png';
+import personalSafetyStamp from './assets/stamps/Personal_Safety.png';
+import shelterStamp from './assets/stamps/Shelter.png';
+import waterAndSanitationStamp from './assets/stamps/Water_Sanitation.png';
 
 let definitionsCSV = require('./assets/definitions-2021.csv');
 
@@ -35,6 +46,27 @@ let definitionSwitch = (d) => {
   }
 };
 
+let stampSwitch = (d) => {
+  switch (d[0].replace(/ /g, "_")) {
+    case "Nutrition_and_Basic_Medical_Care": return nutritionAndMedicalStamp;
+    case "Water_and_Sanitation": return waterAndSanitationStamp;
+    case "Shelter": return shelterStamp;
+    case "Personal_Safety": return personalSafetyStamp;
+
+    case "Access_to_Basic_Knowledge": return advancedEducationStamp;
+    case "Access_to_Information_and_Communications": return infoAndCommunicationsStamp;
+    case "Health_and_Wellness": return healthAndWellnessStamp;
+    case "Environmental_Quality": return environmentalQualityStamp;
+
+    case "Personal_Rights": return personalRightsStamp;
+    case "Personal_Freedom_and_Choice": return personalFreedomStamp;
+    case "Inclusiveness": return inclusivenessStamp;
+    case "Access_to_Advanced_Education": return advancedEducationStamp;
+
+    default: return '';
+  }
+}
+
 function App() {
 
   let ref = useRef();
@@ -48,9 +80,9 @@ function App() {
   function Draw(data) {
     let groupedData = d3.group(data, d => d["Dimension"], d => d["Component"], d => d['Indicator name']);
 
-    let defDiv = d3.select(ref.current);
+    let d3ref = d3.select(ref.current);
     console.log(groupedData);
-    defDiv.selectAll('.dimension')
+    let dimensionDiv = d3ref.selectAll('.dimension')
       .data(groupedData, d => d[0])
       .join(
         enter => {
@@ -87,28 +119,34 @@ function App() {
               default: return;
             }
           }).attr('class', 'dimension_img');
+          let componentGroup = dimDiv
+            .append('div').attr('class', 'component-box')
+            .selectAll('.component')
+            .data(d => d[1])
+            .join(
+              enter => enter.append('div').attr("class", "component").attr("id", d => {
+                let parsedId = d[0].replace(/ /g, "_");
+                return parsedId;
+              }),
+              exit => exit.remove()
+            );
+          componentGroup
+          .append("img").attr("src", (d, i) => {
+            console.log(d, d[0]);
+            console.log(stampSwitch(d));
+            return stampSwitch(d);
+          }).attr('class', 'component-img');  
+          
 
           return enter;
         });
-
 
 
     console.log(groupedData.get('Component'));
     function addComponents(event, d) {
       console.log(event, d[1]);
       console.log(this);
-      d3.select(this.parentNode)
-        .append('div').attr('class', 'component-box')
-        .selectAll('.component')
-        .data(d[1])
-        .join(
-          enter => enter.append('div').attr("class", "component").attr("id", d => {
-            let parsedId = d[0].replace(/ /g, "_");
-            return parsedId;
-          }),
-          exit => exit.remove()
-        );
-        
+
     };
 
     // dimensionsDiv.call(addComponents)
